550b7d91b6a40921e260d83087eb9f839db6886a
jgoode07 <joegoode.07@gmail.com>
2025-09-10 16:02:11 -0700

    Create DiscountSystem class
    Add scanner, welcome message and job title prompt

---
+++ DiscountSystem.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class DiscountSystem {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("Welcome to the Employee Discount Calculator!");
+        System.out.println("Please enter your job title: ");
+        String jobTitle = scnr.nextLine();
+    }
+}
+
